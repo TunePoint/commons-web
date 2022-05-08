f2d30ef4e67a81e578a48543ce461db4c8046aec
Kovalev Danil <opana.loog@gmail.com>
2022-05-09 01:05:52 +0300

    add creating method

--- commons-web-model/src/main/java/ua/tunepoint/web/model/IdResponse.java
+++ commons-web-model/src/main/java/ua/tunepoint/web/model/IdResponse.java
@@ -6,4 +6,8 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @NoArgsConstructor
 public class IdResponse extends CommonResponse<IdPayload> {
+
+    public static IdResponse withId(Long id) {
+        return IdResponse.builder().payload(new IdPayload(id)).build();
+    }
 }
